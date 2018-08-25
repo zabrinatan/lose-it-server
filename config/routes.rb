27d356c278ab13afc17eb8cf12af3cd782585cfd
Zabrina L <zabrinatan@hotmail.com>
2018-08-25 18:50:33 +1000

    added controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
