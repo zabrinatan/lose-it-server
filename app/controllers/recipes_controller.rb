cac2bbf2f0d1886acdbe7ab25adc46200400bea5
Zabrina L <zabrinatan@hotmail.com>
2018-08-26 15:25:01 +1000

    post request to backend to send json object of recipe thats been added to db

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,10 +6,8 @@ class RecipesController < ApplicationController
   def apicall
     q = params[:q]
     @recipe = Recipe.new
-    recipe_url = "https://api.edamam.com/search?q=#{q}&app_id=834d210a&app_key=5746ad44a220392bdfb46f4ed052f6e4&from=0&to=10&calories=591-722&health=alcohol-free"
+    recipe_url = "https://api.edamam.com/search?q=#{q}&app_id=834d210a&app_key=5746ad44a220392bdfb46f4ed052f6e4&from=0&to=20&calories=591-722&health=alcohol-free"
     recipe_info = HTTParty.get recipe_url
-    recipe_name = recipe_info['hits'][0]['recipe']['label'];
-
     # @recipe.label = recipe_name;
     # @recipe.image = recipe_info['hits'][0]['recipe']['image']
     # @recipe.url = recipe_info['hits'][0]['recipe']['url']
@@ -18,21 +16,30 @@ class RecipesController < ApplicationController
     # @recipe.calories = recipe_info['hits'][0]['recipe']['calories']
 
         respond_to do |format|
-        if @recipe.save
+
           # format.html { redirect_to user, notice: "User was successfully created."}
           format.json { render :json => recipe_info }
-        else
-          # format.html { render :new }
-          format.json { render json: @recipe.errors, status: :unprocessable_entity }
-        end
+
         end
       end
 
   def new
 
   end
+  def create
+    @recipe = Recipe.new
+    @recipe.label = params[:data]['recipe']['label'];
+    @recipe.image = params[:data]['recipe']['image'];
+    @recipe.url = params[:data]['recipe']['url']
+    @recipe.yield = params[:data]['recipe']['yield']
+    @recipe.totalTime = params[:data]['recipe']['totalTime']
+    @recipe.calories = params[:data]['recipe']['calories']
+    @recipe.save
+
+  end
 
   def show
+
 
   end
 
